2f95dd503b2ea7aca02e6596d052b42232332f4c
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-06-24 17:58:20 +0100

    BlogPosting Schema

--- single.php
+++ single.php
@@ -1,9 +1,57 @@
 <?php
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
+$img = get_the_post_thumbnail(get_the_ID(), 'full', array('class' => 'single-blog__image'));
+
+
+$logo = get_stylesheet_directory_uri() . '/img/belmont-logo-full.png';
+$post_title = get_the_title();
+$post_thumbnail = get_the_post_thumbnail_url( get_the_ID(), 'full' );
+$yoast_meta_description = get_post_meta( get_the_ID(), '_yoast_wpseo_metadesc', true );
+$publication_date = get_the_date('c');
+$modification_date = get_the_modified_date('c');
+$permalink = get_permalink();
+if (empty($yoast_meta_description)) {
+    $yoast_meta_description = wp_trim_words(get_the_excerpt(), 30, '...');
+}
+
+$schema = <<<EOT
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "BlogPosting",
+  "headline": "$title",
+  "image": "$post_thumbnail",
+  "author": {
+    "@type": "Person",
+    "name": "Alease Parlanti"
+  },
+  "publisher": {
+    "@type": "Organization",
+    "name": "Belmont Skin and Laser Clinic",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "$logo"
+    }
+  },
+  "datePublished": "$publication_date",
+  "dateModified": "$modification_date",
+  "description": "$yoast_meta_description",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "$permalink"
+  }
+}
+</script>
+EOT;
+
+add_action('wp_head',function() {
+    global $schema;
+    echo $schema;
+});
+
 get_header();
 // $img = get_the_post_thumbnail_url(get_the_ID(),'full');
-$img = get_the_post_thumbnail(get_the_ID(), 'full', array('class' => 'single-blog__image'));
 
 ?>
 <main id="main" class="single-blog">
